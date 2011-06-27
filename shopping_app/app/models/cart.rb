72546e6668e5bb2da5312d5dec6a75d4b9e6cb50
marshjf5 <marshjf5@gmail.com>
2011-06-27 15:25:26 +0800

    5

--- shopping_app/app/models/cart.rb
+++ shopping_app/app/models/cart.rb
@@ -1,7 +1,13 @@
 class Cart < ActiveRecord::Base
-  attr_reader:purchase
+  
   attr_reader:price
-  def add_purchase
-
+  def add_purchase(item)
+    appendFlag = true
+    for purchase in @purchase
+      if(item.id == item.purchase.id)
+        appendFlag = false
+        purchase.quantity += 1
+      end
+    end
   end
 end
