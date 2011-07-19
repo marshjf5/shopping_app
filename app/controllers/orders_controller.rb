1a7421611fd00253b7e3d138e0a1dba958175739
marshjf5 <marshjf5@gmail.com>
2011-07-19 11:48:20 +0800

    change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,35 +1,28 @@
 class OrdersController < ApplicationController
   def update_amount
-    cart
-    id=rand(999999999999999999)
-    a=params[:order][:commodity_id_array]
- 	  a=a[1..-2]
- 	  b=a.split(", ")
-    @package_ids=[]
-    @supplier_names=[]
-    exist=1
- 	  b.each do |commodity_id|
- 	    @commodity=Commodity.find(commodity_id)
-      @supplier = @commodity.supplier
- 	    @supplier_names<<""+@supplier.name+id.to_s
-      @supplier_names.each do |supplier_name|
-        if (Package.find_by_name(supplier_name)==nil)
-          exist=0
+     a=params[:order][:commodity_id_array]
+ 	   a=a[1..-2]
+     b=a.split(", ")
+    @com = Commodity.find(b)
+    @suppliers=[]
+    @com.each do |commodity| unless @suppliers.include?(commodity.supplier)
+        @suppliers<<commodity.supplier
+      end
+      hash={}
+      @packages=[]
+      @suppliers.each do |supplier|
+        @package = Package.create(:name=>supplier.name, :supplier_id=>supplier.id)
+               @packages<<@package
+        hash[supplier.id]=@package.id
+      end
+      @com.each do |commodity|
+        @price = commodity.price.to_i * (params["value#{commodity.id}"].to_i) + Supplier.find(commodity.supplier_id).postage
+        if (@price > 5000)
+          @price = commodity.price * (params["value#{commodity.id}"].to_i)
         end
- 	    end
- 	    if(exist==0)
- 	      @package = Package.create(:name=>""+@supplier.name+id.to_s,:supplier_id =>@supplier.id)  #生成包裹
- 	      @package_ids<<@package.id
-        exist=1
+        @order = Order.create(:package_id =>hash[commodity.supplier_id],:commodity_id=>commodity.id,:amount=>params["value#{commodity.id}"],:order_price=>@price)
       end
-      @length = params["value#{commodity_id}"].to_i
-      @price = @commodity.price * @length + @commodity.supplier.postage
- 	    if (@price > 5000)
-        @price = @commodity.price * @length
-      end
-      @order = Order.create(:package_id =>@package.id,:commodity_id=>@commodity.id,:amount=>params["value#{commodity_id}"],:order_price=>@price)
     end
     render "/suppliers/suppliers"
-
   end
 end
