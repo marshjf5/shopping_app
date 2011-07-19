b67fcf482aa8858f46a444300194c07b6a3a98f0
marshjf5 <marshjf5@gmail.com>
2011-07-19 14:58:15 +0800

    123

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   def update_amount
-     a=params[:order][:commodity_id_array]
- 	   a=a[1..-2]
-     b=a.split(", ")
+    a=params[:order][:commodity_id_array]
+    a=a[1..-2]
+    b=a.split(", ")
     @com = Commodity.find(b)
     @suppliers=[]
     @com.each do |commodity| unless @suppliers.include?(commodity.supplier)
@@ -12,15 +12,30 @@ class OrdersController < ApplicationController
       @packages=[]
       @suppliers.each do |supplier|
         @package = Package.create(:name=>supplier.name, :supplier_id=>supplier.id)
-               @packages<<@package
+        @packages<<@package
         hash[supplier.id]=@package.id
+       
       end
       @com.each do |commodity|
-        @price = commodity.price.to_i * (params["value#{commodity.id}"].to_i) + Supplier.find(commodity.supplier_id).postage
-        if (@price > 5000)
-          @price = commodity.price * (params["value#{commodity.id}"].to_i)
-        end
+        @price = commodity.price * (params["value#{commodity.id}"].to_i) 
         @order = Order.create(:package_id =>hash[commodity.supplier_id],:commodity_id=>commodity.id,:amount=>params["value#{commodity.id}"],:order_price=>@price)
+      end
+    end
+
+    
+    @sum =0.0 #未加邮费
+   @total=0.0 #总和
+    @packages.each do |package|
+      package.orders.each do |order|
+        @sum+=order.order_price.to_i
+      end
+      if (@sum>5000)
+          @total=@sum
+    else
+      @suppliers.each do |supplier|
+        @sum+=supplier.postage
+        @total=@sum
+      end
       end
     end
     render "/suppliers/suppliers"
